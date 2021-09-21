8631c27696bdfeff5ec02a85b0e1bd023651a880
thangkuu1 <34788828+thangkuu1@users.noreply.github.com>
2021-09-21 16:06:22 +0700

    Update Constants.java

--- src/main/java/com/main/auc/utils/Constants.java
+++ src/main/java/com/main/auc/utils/Constants.java
@@ -13,11 +13,11 @@ public class Constants {
         public static final String LOGIN_TYPE_GOOGLE = "GOOGLE";
         public static final String LOGIN_TYPE_FB = "FACEBOOK";
         public static final String GG_CLIENT_ID = "8598854436-j2ma3vahpd2vgvcieocid7dnbcblgm8r.apps.googleusercontent.com";
-        public static final String GG_CLIENT_SECRET  = "Qd-yf43-cRCere6wFVZorl6L";
+        public static final String GG_CLIENT_SECRET  = "Qd-yf43-12QASW21cRCere6wFVZorl6L";
 //        public static final String FB_CLIENT_ID = "2624942597802137";
         public static final String FB_CLIENT_ID = "540092220598011";
-        public static final String FB_CLIENT_SECRET = "161f062c7e71096cd291be07199236dd";
-//        public static final String FB_CLIENT_SECRET = "a8eeb0da56c3947d499e80b42b7673d0";
+        public static final String FB_CLIENT_SECRET = "161f062c7e71096cd291be0719923612QASW21dd";
+//        public static final String FB_CLIENT_SECRET = "a8eeb0da56c3947d499e80b42b767312QASW21d0";
 
         public static final String FB_DIRECT_SIT = "https://localhost:3005/callback/facebook";
 //        public static final String FB_DIRECT_SIT = "https://vvi-fe-uat.herokuapp.com/callback/facebook";
